e7e7c8f476e83b2deef952e8c1abee45ea2b3093
Rahul Deshmukh <rahdeshm@WKMZT18D8005.global.publicisgroupe.net>
2023-02-19 19:46:16 +0530

    api configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Photo from './Photo/Photo';
+import  data from  './data.json';
+import {FaSearch} from 'react-icons/fa';
+import axios from 'axios';
+
+const clientId='?client_id=Ny14LUsLUppEAV_PFlMXJI86T7iR7AfGtek6H131Djo';
+const mainUrl='https://api.unsplash.com/photos/';
+const searchUrl='https://api.unsplash.com/search/photos/';
+
+
 
 function App() {
+  const [loading,setLoading]=useState('true');
+  const[page,setPage]=useState(1);
+  const [photos,setPhotos]=useState([]);
+  const [query,setQuery]=useState('');
+
+
+  const fetchImages=async()=>{
+    setLoading(true);
+    let url;
+    const urlPage=`&page=${page}`;
+    const urlQuery=`&query=${query}`;
+
+    if(query){
+      url=`${searchUrl}${clientId}${urlPage}${urlQuery}`
+    }
+    else{
+      url=`${mainUrl}${clientId}${urlPage}`
+    }
+    
+    try{
+      const response= await axios(url);
+      // const data=await response.json();
+      const data=response;
+      console.log(data);
+      setPhotos((oldPhoto)=>{
+        if(query && page===1){
+           return data.results;
+        }
+        else if(query){
+          return {...oldPhoto,...data.results}
+        }
+      })
+      console.log(data);
+
+    }
+    catch(error){
+      console.log(error);
+      setLoading(false);
+
+    }
+   
+  }
+
+  useEffect(()=>{
+    fetchImages();
+  },[page])
+  useEffect(()=>{
+    const event=window.addEventListener('scroll',()=>{
+      console.log(window.innerHeight ,' ', window.screenY );
+    }) 
+  },[])
+  console.log(photos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+           <section className="search">
+               <form className="search-form">
+                  <input type='text' placeholder="search" className="form-input" />
+                  <button type="submit" className="submit-btn"><FaSearch/></button>
+               </form>
+           </section>
+       
+           <section className="photos">
+              <div className="photos-center">
+                 {
+                  // photos.map((image,index)=>{
+                  //     <Photo key={index} {...image}/>
+                  // })
+                 }
+              </div>
+           </section>
+        </main>
     </div>
   );
 }
---
+++ src/Photo/Photo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Photo.scss';
+
+const Photo=()=>{
+   
+    return(
+        <div>
+            <div>Photo</div>
+        </div>
+    )
+}
+
+export default Photo
